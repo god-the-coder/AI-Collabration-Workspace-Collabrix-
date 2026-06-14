dbf172cfab7854258e3a9fa6ab5ef5ad39367db9
god-the-coder <mrgodninjaq@gmail.com>
2026-06-14 18:52:24 +0530

    accounts models registered in admin site

--- backend/apps/accounts/admin.py
+++ backend/apps/accounts/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import UserModel
+from .models import SessionsModel 
+from .models import UserSettingsModel as user_setti
+from .models import EmailVerificationTokenModel as email_veri_token
+from .models import PasswordResetTokenModel as pass_reset_token
 
 # Register your models here.
 admin.site.register(UserModel)
+admin.site.register(SessionsModel)
+admin.site.register(user_setti)
+admin.site.register(pass_reset_token)
+admin.site.register(email_veri_token)
